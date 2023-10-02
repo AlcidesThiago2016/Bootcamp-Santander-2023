57b790c21b201209f3b02b4329d8792d8e83999e
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2023-10-02 15:20:03 -0300

    OnInit

--- Angular/Angular Modulos/life-cycle/src/app/title/title.component.ts
+++ Angular/Angular Modulos/life-cycle/src/app/title/title.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component ,  OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent {
+export class TitleComponent implements OnInit{
 
+  nome:String = 'Alcides'
+
+  constructor(){}
+
+  ngOnInit(): void {
+    this.nome = `Ola ${this.nome}`
+  }
 }
